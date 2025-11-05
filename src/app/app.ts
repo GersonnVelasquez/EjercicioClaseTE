8e843d71e6433a696d60d1ce42a88c1d161c0b0c
Gersonn Velasquez <31450496+GersonnVelasquez@users.noreply.github.com>
2025-11-04 20:54:42 -0600

    añadiendo formulario (#1)

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,30 +2,66 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Persona } from './services/persona';
 import { IPersona } from './models/persona';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('EjercicioClase');
   personaService = inject(Persona);
+  formBuilder = inject(FormBuilder);
   personas: IPersona[] = [];
+
+  personaForm = this.formBuilder.group({
+    nombre: ['', [Validators.required],[]],
+    edad: [0, [Validators.required, Validators.min(12)]],
+    email: ['', [Validators.required, Validators.email]],
+    id: [null],
+  });
 
   constructor() {
     this.obtenerPersonas();
   }
 
-  async agregarPersona() {
-    await this.personaService.create({
-      nombre: 'Juan Perez',
-      edad: 30,
-      email: 'juan.perez@example.com',
-    });
+  async submitForm() {
+    if (this.personaForm.invalid) {
+      alert('Formulario inválido. Por favor, verifica los datos ingresados.');
+      return;
+    }
 
-    alert('Persona agregada');
+    const estaActualizando = this.personaForm.value.id;
+
+    if (estaActualizando) {
+      this.personaService.update({
+        id: this.personaForm.value.id!,
+        nombre: this.personaForm.value.nombre!,
+        edad: this.personaForm.value.edad!,
+        email: this.personaForm.value.email!,
+      });
+    } else {
+      await this.personaService.create({
+        nombre: this.personaForm.value.nombre!,
+        edad: this.personaForm.value.edad!,
+        email: this.personaForm.value.email!,
+      });
+    }
+
+    this.personaForm.reset();
+  }
+
+  borrarPersona(id: string | undefined) {
+    if (id) {
+      this.personaService.delete(id);
+    }
+  }
+
+  editarPersona(persona: IPersona) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    this.personaForm.patchValue(persona as any);
   }
 
   obtenerPersonas() {
